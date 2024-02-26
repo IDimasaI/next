980b1b7f5803b46002cb47057937e4a7017a22df
IDimasaI <137656810+IDimasaI@users.noreply.github.com>
2024-02-26 10:46:03 +0300

    cah new

--- pages/region/[id].tsx
+++ pages/region/[id].tsx
@@ -7,24 +7,24 @@ interface RegionPageProps {
 
 export default function RegionPage({ id }: RegionPageProps) {
   return <h1>Страница Region с id: {id}</h1>;
-  }
-  
-  export const getServerSideProps: GetServerSideProps<RegionPageProps> = async (context) => {
+}
+
+export const getServerSideProps: GetServerSideProps<RegionPageProps> = async (context) => {
   const { id } = context.query;
   const isValidSlug: boolean = /^[A-Za-z0-9_]+$/.test(id as string);
   if (!isValidSlug) {
-  return {
-  props: {
-  id: 'Error'
-  },
-  revalidate: 60, // Enable caching for 60 seconds
+    return {
+      props: {
+        id: 'Error'
+      },
+      revalidate: 60, // Enable caching for 60 seconds
+    }
   }
-  }
-  
+
   return {
-  props: {
-  id: id as string,
-  },
-  revalidate: 60, // Enable caching for 60 seconds
+    props: {
+      id: id as string,
+      revalidate: 60, // Enable caching for 60 seconds
+    },
   };
-  };
+};
